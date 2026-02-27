5a703a4fc192a63a0bff908dd7b6347590b304f4
avverma9 <av95766@gmail.com>
2026-02-27 21:40:05 +0530

    done

--- front/app/lib/baseUrl.js
+++ front/app/lib/baseUrl.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.NEXT_PUBLIC_SITE_API_BASE_URL || "http://sarkariafsar.com/api/site";
+const baseUrl = process.env.NEXT_PUBLIC_SITE_API_BASE_URL || "https://sarkariafsar.com/api/site";
 
 export default baseUrl;
