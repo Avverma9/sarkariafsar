8902ae52efc9cd147900244bb3116abb64490bf1
avverma9 <av95766@gmail.com>
2026-04-13 22:21:15 +0530

    done

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "sarkariafsar-api",
-      cwd: "/home/ubuntu/apps/sarkariafsar/api",
+      cwd: "/home/ubuntu/apps/sarkariafsar/server",
       script: "index.js",
       env: {
         NODE_ENV: "production",
